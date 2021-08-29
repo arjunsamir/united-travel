56585f165a7a7e8023265d136fbe9875641aa532
Arjun Patel <me@arjunsamir.com>
2021-08-29 00:28:08 -0400

    Removed legacy code

--- client/src/js-legacy/BookingApp/Login/hooks/useObjectState.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export default function useObjectState(initialState) {
-
-    const [state, setState] = useState(initialState);
-
-    return [state, (newState) => setState(Object.assign({}, state, newState))]
-
-}
